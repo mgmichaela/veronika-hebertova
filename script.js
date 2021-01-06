592be5d895410d9dc4de3852906a4920476884d7
Michaela Guzmická <mgmichaelaguzmicka@gmail.com>
2021-01-06 16:46:50 +0100

    add tooltip

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-new Glide('.glide', {
-    type: 'carousel',
-    startAt: 0,
-    perView: 3,
-    focusAt: 'center'
-  }).mount()
+//glidejs
+new Glide(".glide", {
+  type: "carousel",
+  startAt: 0,
+  perView: 3,
+  focusAt: "center",
+}).mount();
+
+//tooltip
+function show(elem) {
+  elem.style.display = "block";
+}
+function hide(elem) {
+  elem.style.display = "";
+}
